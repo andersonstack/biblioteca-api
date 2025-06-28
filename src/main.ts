d20b419af7e6b12c14c232376fd5d5f7d1356c96
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-28 19:16:40 -0300

    feat: método post para adicionar usuário no banco de dados

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import {createUserInBD} from './database/db';
 
 const app = express();
 app.use(cors());
@@ -8,3 +9,13 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log("[!] Back-end rodando!")
 });
+
+app.post('/usuario', async (req, res) => {
+    const {user, nome, senha} = req.body;
+    await createUserInBD(user, nome, senha);
+    if (res.status(201)){
+        console.log('Requisição concluída');
+    } else {
+        console.log('Falha na requisição');
+    }
+})
