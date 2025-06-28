7e79c02e4332539b1a1893163b9db50bd5302f68
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-28 19:08:55 -0300

    feat: função de criar novo usuário

--- src/database/db.ts
+++ src/database/db.ts
@@ -13,3 +13,28 @@ const connectionDB =  async () => {
         console.log(`Erro ao conectar com o banco de dados: ${error}`)
     }
 }
+
+export const createUserInBD = async (
+    user: String,
+    nome: String,
+    senha: String,
+    ) => {
+
+    const connection = await connectionDB();
+
+    if (connection != undefined) {
+        try {
+            const query = `
+                INSERT INTO usuario (user_name, name, senha)
+                    VALUES (?, ?, ?)
+            `;
+            await connection.execute(query, [user, nome, senha]);
+            console.log(`Usuário '${user} adicionado com sucesso!'`);
+            connection.end();
+        } catch (error) {
+            console.log(`Erro ao criar o usuário ${user}: ${nome}`);
+            connection.end();
+        }
+    }
+
+}
