0146f57e1b54dda89e2e4ec7835f07e2a0071a89
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-02 13:50:43 -0300

    chore: importando as rotas

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
-
 dotenv.config();
 export const app = express();
 app.use(cors());
 app.use(express.json());
 
+import "./service/rota_emprestimos";
+import "./service/rota_livro";
+import "./service/rota_usuario";
 
 app.listen(3000, () => {
   console.log("[!] Back-end rodando!");
