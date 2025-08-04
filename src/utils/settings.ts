1b7471250c48b3a6fb628530c29751a51e5ab975
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-08-04 17:48:28 -0300

    fix: ajuste no caminho do 'upload'

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -3,31 +3,31 @@ import path from 'path';
 import fs from 'fs';
 
 export async function baixarImagem(imagemLink: string): Promise<string> {
-    try {
-      const response = await axios.get(imagemLink, {responseType: 'stream'});
-      
-      const validExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
-      const ext = path.extname(imagemLink.split('?')[0]).toLocaleLowerCase();
-      if (!validExtensions.includes(ext))
-          return "";
+  try {
+    const response = await axios.get(imagemLink, { responseType: 'stream' });
 
-      const nomeArquivo = Date.now() + path.extname(imagemLink.split('?')[0]);
+    const validExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
+    const ext = path.extname(imagemLink.split('?')[0]).toLocaleLowerCase();
+    if (!validExtensions.includes(ext)) return "";
 
-      const caminhoCompleto = path.join(__dirname, 'uploads', nomeArquivo);
-      const writer = fs.createWriteStream(caminhoCompleto);
-  
-      response.data.pipe(writer);
-  
-      await new Promise((resolve, reject) => {
-        writer.on('finish', () => resolve(undefined));
-        writer.on('error', reject);
-      });
+    const nomeArquivo = Date.now() + ext;
 
-      return `/uploads/${nomeArquivo}`;
+    const caminhoUploads = path.resolve(__dirname, '..', 'uploads');
+    fs.mkdirSync(caminhoUploads, { recursive: true });
 
-    } catch (error) {
-      console.log(error);
-      return "";
-    }
+    const caminhoCompleto = path.join(caminhoUploads, nomeArquivo);
+    const writer = fs.createWriteStream(caminhoCompleto);
+
+    response.data.pipe(writer);
+
+    await new Promise((resolve, reject) => {
+      writer.on('finish', () => resolve(undefined));
+      writer.on('error', reject);
+    });
+
+    return `/uploads/${nomeArquivo}`;
+  } catch (error) {
+    console.log(error);
+    return "";
+  }
 }
- 
